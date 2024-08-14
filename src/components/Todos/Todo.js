fc1780d6703ecb22418fd383926b27dfe40e111f
”ukrWily” <ukrwily@gmail.com>
2024-08-14 21:10:03 +0300

    PC first

---
+++ src/components/Todos/Todo.js
@@ -0,0 +1,4 @@
+function Todo() {
+  return <div></div>;
+}
+export default Todo;
---
+++ src/components/Todos/TodoForm.js
@@ -0,0 +1,9 @@
+function TodoForm() {
+  return (
+    <form>
+      <input type="text" placeholder="Enter new todo" />
+      <button>Submit</button>
+    </form>
+  );
+}
+export default TodoForm;
